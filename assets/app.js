46833cd6effb390097c59ef0e0efa28173a7e771
BritishPandaChick <sbartley1080@gmail.com>
2019-07-12 20:48:36 -0400

    Add new div tags for modal header, body and footer. Update CSS styles for modal elements. Add JS variables to get modal elements. Start adding JS code to get modal to open and close

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,14 @@
-$(document).ready(function() {
-  $("#myModal").modal();
-});
+//Variables
+var modal = document.getElementById("#myModal");
+var btn = document.getElementById("#rsvp");
+var close = document.getElementsByClassName(".close")[0];
+
+//User clicks on button and opens modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+//User clicks on x and closes modal
+close.onclick = function() {
+  modal.style.display = "none";
+}
