cdf093e53e0dec0124593fc6dd5d8f7a43118122
BritishPandaChick <sbartley1080@gmail.com>
2019-07-13 20:01:25 -0400

    Add document.ready(function()) for jquery. Remove punctuation for document.getElementById in variables.

--- assets/app.js
+++ assets/app.js
@@ -1,14 +1,23 @@
-//Variables
-var modal = document.getElementById("#myModal");
-var btn = document.getElementById("#rsvp");
-var close = document.getElementsByClassName(".close")[0];
+$(document).ready(function(){
+  //Variables
+  var modal = document.getElementById("myModal");
+  var rsvpButton = document.getElementById("rsvpButton");
+  var close = document.getElementsByClassName("close")[0];
 
-//User clicks on button and opens modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
+  //User clicks on button and opens modal
+  rsvpButton.onclick = function() {
+    modal.style.display = "block";
+  }
 
-//User clicks on x and closes modal
-close.onclick = function() {
-  modal.style.display = "none";
-}
+  //User clicks on x and closes modal
+  close.onclick = function() {
+    modal.style.display = "none";
+  }
+
+  //User clicks anywhere outside modal, closes it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+});
